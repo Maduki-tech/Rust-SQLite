e468fa5b3783f38fac879563faee525b7a4cf772
Maduki-tech <d.schlueter1011@gmail.com>
2023-12-21 19:11:11 +0100

    added tupple

--- src/sql_handler/sql.rs
+++ src/sql_handler/sql.rs
@@ -1,24 +1,33 @@
+// TODO: add rows to the sql
 pub struct Sql {
+    data: Vec<Row>,
+}
+
+struct Row {
     name: String,
     age: i32,
 }
 
 impl Sql {
+    /// Create a new [`SQL`] struct
     pub fn new() -> Sql {
-        Sql {
-            name: String::new(),
-            age: 0,
-        }
+        Sql { data: Vec::new() }
     }
 
+    /// set the values of the [`SQL`] struct
+    ///
+    /// * `name`:
+    /// * `age`:
     pub fn set_values(&mut self, name: String, age: i32) {
-        self.name = name;
-        self.age = age;
-
+        self.data.push(Row { name, age });
     }
 
-    pub fn get_values(&self) -> (String, i32) {
-        (self.name.clone(), self.age)
+    /// Get the values of the [`SQL`] struct
+    pub fn get_values(&self) -> Vec<(String, i32)> {
+        return self.data
+            .iter()
+            .map(|row| (row.name.clone(), row.age))
+            .collect::<Vec<(String, i32)>>();
     }
 }
 
@@ -30,6 +39,6 @@ mod tests {
     fn test_set_values() {
         let mut sql = Sql::new();
         sql.set_values(String::from("test"), 1);
-        assert_eq!(sql.get_values(), (String::from("test"), 1));
+        assert_eq!(sql.get_values(), vec![(String::from("test"), 1)]);
     }
 }
